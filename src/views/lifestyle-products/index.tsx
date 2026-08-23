fbd9395cc78a9f78197faeb4c7e57b80b168ce99
rayhan926 <rayhanbd7075@gmail.com>
2026-08-23 16:38:19 +0600

    Comment out HeroSection import and usage in LifestyleProducts component

--- src/views/lifestyle-products/index.tsx
+++ src/views/lifestyle-products/index.tsx
@@ -1,12 +1,12 @@
-import BannersSection from "@/views/lifestyle-products/components/banners-section";
-import CategoriesSection from "@/views/lifestyle-products/components/categories-section";
-import HeroSection from "@/views/lifestyle-products/components/hero-section";
-import ProductsByCategorySection from "@/views/lifestyle-products/components/products-by-category-section";
+import BannersSection from '@/views/lifestyle-products/components/banners-section';
+import CategoriesSection from '@/views/lifestyle-products/components/categories-section';
+// import HeroSection from '@/views/lifestyle-products/components/hero-section';
+import ProductsByCategorySection from '@/views/lifestyle-products/components/products-by-category-section';
 
 const LifestyleProducts = () => {
   return (
     <>
-      <HeroSection />
+      {/* <HeroSection /> */}
       <CategoriesSection />
       <ProductsByCategorySection />
       <BannersSection />
